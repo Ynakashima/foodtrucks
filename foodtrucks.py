537bf19e8a501418772cc6f849c0b993ba603075
Yuri Nakashima <yuri310@gmail.com>
2017-05-31 23:44:59 -0700

    added imports and def open_today

--- foodtrucks.py
+++ foodtrucks.py
@@ -1,6 +1,34 @@
 import requests
+import sodapy
+import datetime
 
-url = "https://data.sfgov.org/resource/bbb8-hzi6.json"
+now = datetime.datetime.now()
+hour_now = now.strftime("%H")
+today = "{}".format(now.strftime("%A"))
+url = "https://data.sfgov.org/resource/bbb8-hzi6.json?"
 response = requests.get(url)
 if response.status_code == 200:
-    print response.text
+    response_list = response.json()
+
+
+# def all_foodtrucks():
+#     for each_truck in response_list:
+#         print("Name:{}  Address:{} Start:{} End:{}".format(
+#             each_truck.get('applicant'),
+#             each_truck.get('location'),
+#             each_truck.get('start24'),
+#             each_truck.get('end24'),
+#             )
+#         )
+
+
+# all_foodtrucks()
+
+def open_today():
+    for each_truck in response_list:
+        if (each_truck.get('dayofweekstr') == today:
+        print each_truck.get('applicant')
+        print each_truck.get('dayofweekstr')
+
+
+open_today()
